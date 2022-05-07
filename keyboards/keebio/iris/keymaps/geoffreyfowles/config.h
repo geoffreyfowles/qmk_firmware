b600f0da0e102a5dafb6caa72ab1a628ea978e5b
Geoffrey Fowles <40678571+geoffreyfowles@users.noreply.github.com>
2022-05-07 13:15:25 -0400

    change mouse key dynamics

--- keyboards/keebio/iris/keymaps/geoffreyfowles/config.h
+++ keyboards/keebio/iris/keymaps/geoffreyfowles/config.h
@@ -28,11 +28,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // mouse keys
 // TODO use kinetic once implemented properly
-#define MOUSEKEY_INTERVAL 16
 #define MOUSEKEY_DELAY 0
-#define MOUSEKEY_TIME_TO_MAX 60
-#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_INTERVAL 16
 #define MOUSEKEY_WHEEL_DELAY 0
+#define MOUSEKEY_MAX_SPEED 6
+#define MOUSEKEY_TIME_TO_MAX 64
 
 // for home row modifiers
 #define TAPPING_TERM 250
